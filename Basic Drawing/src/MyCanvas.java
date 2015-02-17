ae49888563dba793d304cf94fee1d24436645ec7
micaela.jones <micaela.jones@MU-C3-01.mullen.pvt.k12.co.us>
2015-02-17 09:01:01 -0700

    did the smiley face with blinking

--- Basic Drawing/src/MyCanvas.java
+++ Basic Drawing/src/MyCanvas.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+
 import javax.swing.*;
 
 public class MyCanvas extends Canvas 
@@ -9,15 +10,51 @@ public class MyCanvas extends Canvas
     		{
 	        MyCanvas canvas = new MyCanvas();
 	        JFrame frame = new JFrame();
-	        frame.setSize(400, 400);
+	        frame.setSize(1000, 1000);
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	        frame.getContentPane().add(canvas);
 	        frame.setVisible(true);
+	        
+	        
     		}
+    public void paint(Graphics graphics)
+    	{
+    	graphics.setColor(Color.orange);
+    	graphics.drawRoundRect(250, 250, 500, 500, 500, 500);
+    	graphics.fillRoundRect(250, 250, 500, 500, 500, 500);
+    	graphics.setColor(Color.cyan);
+    	graphics.fillRect(400, 400, 50, 50);
+    	graphics.fillRect(600, 400, 50, 50);
+    	graphics.setColor(Color.darkGray);
+    	graphics.fillRoundRect(300, 500, 400, 100, 100, 150);
+    	graphics.setColor(Color.red);
+    	graphics.fillRoundRect(350, 550, 100, 200, 200, 150);
+    	for (int i = 0; i < 600; i++)
+			{
+			graphics.setColor(Color.orange);
+			graphics.fillRect(400, 400, 50, 50);
+			graphics.fillRect(600, 400, 50, 50);
+			graphics.setColor(Color.black);
+			graphics.drawLine(400, 400, 450, 400);
+			graphics.drawLine(600, 400, 650, 400);
+			try
+				{
+				Thread.sleep(500);
+				} catch (InterruptedException e)
+				{
+				e.printStackTrace();
+				}
+			graphics.setColor(Color.cyan);
+			graphics.fillRect(400, 400, 50, 50);
+	    	graphics.fillRect(600, 400, 50, 50);
+	    	try
+				{
+				Thread.sleep(2000);
+				} catch (InterruptedException e)
+				{
+				e.printStackTrace();
+				}
+			}
+    	}
 
-    public void paint(Graphics graphics) 
-    		{
-	        graphics.setColor(Color.black);
-	        graphics.drawLine(40, 30, 330, 380);
-    		}
     }
